db4a5c8b3bcc258a18aa6816ae32642663078488
LukeTandjung <lukelucus123@gmail.com>
2025-10-24 13:50:13 +0100

    Added flake.nix

--- flake.nix
+++ flake.nix
@@ -15,65 +15,59 @@
     }:
     let
       version = "v1.0.0";
-      pkgs = import nixpkgs { inherit system; };
-      systems = import systems;
+      urls = {
+        "x86_64-linux" = "https://cli.leetgpu.com/dist/${version}/leetgpu-linux-amd64";
+        "aarch64-linux" = "https://cli.leetgpu.com/dist/${version}/leetgpu-linux-arm64";
+        "x86_64-darwin" = "https://cli.leetgpu.com/dist/${version}/leetgpu-macos-amd64";
+        "aarch64-darwin" = "https://cli.leetgpu.com/dist/${version}/leetgpu-macos-arm64";
+      };
+      hashes = {
+        "x86_64-linux" = "sha256-GlJzzXkHTrW7Yg4nC1tD1hAlj4YMeveG7DibKu5UDxM=";
+        "aarch64-linux" = "sha256-PA6u/xecC0FWQKECH+GwZJrRTr382RG+tWhLm08IlW0=";
+        "x86_64-darwin" = "sha256-HMP042zVK3DmVxHsPx/sgZr+0k5mFcJD6XiihLqN7wg=";
+        "aarch64-darwin" = "sha256-jEiSHDsLopiYmdsXx36SJKxv1xEI2PRRI6H7ienCskU=";
+      };
+      supported_systems = import systems;
 
-      inherit (pkgs) fetchUrl lib;
-      inherit (pkgs.stdenv) mkDerivation;
       inherit (nixpkgs.lib) genAttrs;
-    in
-    {
-      packages = genAttrs (systems) (
-        system:
-        mkDerivation {
-          inherit version;
+    in {
+      packages = genAttrs (supported_systems) (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-          pname = "leetgpu_cli";
-          src = fetchUrl {
-            url =
-              system:
-              if system == "x86_64-linux" then
-                "https://cli.leetgpu.com/dist/${version}/leetgpu-linux-amd64"
-              else if system == "aarch64-linux" then
-                "https://cli.leetgpu.com/dist/${version}/leetgpu-linux-arm64"
-              else if system == "x86_64-darwin" then
-                "https://cli.leetgpu.com/dist/${version}/leetgpu-macos-amd64"
-              else if system == "aarch64-darwin" then
-                "https://cli.leetgpu.com/dist/${version}/leetgpu-macos-arm64"
-              else
-                throw "Unsupported system: ${system}";
-            sha256 =
-              system:
-              if system == "x86_64-linux" then
-                "sha256-GlJzzXkHTrW7Yg4nC1tD1hAlj4YMeveG7DibKu5UDxM="
-              else if system == "aarch64-linux" then
-                "sha256-PA6u/xecC0FWQKECH+GwZJrRTr382RG+tWhLm08IlW0="
-              else if system == "x86_64-darwin" then
-                "sha256-HMP042zVK3DmVxHsPx/sgZr+0k5mFcJD6XiihLqN7wg="
-              else if system == "aarch64-darwin" then
-                "sha256-jEiSHDsLopiYmdsXx36SJKxv1xEI2PRRI6H7ienCskU="
-              else
-                throw "Unsupported system: ${system}";
+          inherit (pkgs) fetchUrl lib;
+          inherit (pkgs.stdenv) mkDerivation;
+        in {
+          leetgpu_cli = mkDerivation {
+            inherit version;
+
+            pname = "leetgpu_cli";
+            src = fetchUrl {
+              url = urls.${system} or (throw "Unsupported system: ${system}");
+              sha256 = hashes.${system} or (throw "Unsupported system: ${system}");
+            };
+            dontUnpack = true;
+            installPhase = ''
+              mkdir -p "$out/bin"
+              cp "$src" "$out/bin/leetgpu"
+              chmod +x "$out/bin/leetgpu"
+            '';
+            meta = with lib; {
+              description = "LeetGPU CLI";
+              license = licenses.unfree;
+              platforms = platforms.all;
+              mainProgram = "leetgpu";
+            };
           };
-          dontUnpack = true;
-          installPhase = ''
-            mkdir -p "$out/bin"
-            cp "$src" "$out/bin/leetgpu"
-            chmod +x "$out/bin/leetgpu"
-          '';
-          meta = with lib; {
-            description = "LeetGPU CLI";
-            license = licenses.unfree;
-            platforms = platform.all;
-            mainProgram = "leetgpu";
-          };
+
+          default = self.packages.${system}.leetgpu_cli;
         }
       );
 
       homeManagerModules.default =
         { pkgs, ... }:
         {
-          home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system} ];
+          home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.leetgpu_cli ];
         };
     };
 }
